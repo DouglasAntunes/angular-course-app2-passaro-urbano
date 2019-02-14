d0454fcc4e0a999455a65ba334e0f81659f229c9
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-02-14 19:09:54 -0200

    Aula 178

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,12 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = '';
   public formaPagamento: string = '';
 
+  // atributos de controle de validação dos campos
+  public enderecoValido: boolean;
+  public numeroValido: boolean;
+  public complementoValido: boolean;
+  public formaPagamentoValido: boolean;
+
   constructor() { }
 
   ngOnInit() {
@@ -19,22 +25,43 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
-    console.log(this.endereco);
+    // console.log(this.endereco);
+    // se a string for maior q 3
+    if(this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero;
-    console.log(this.numero);
+    // console.log(this.numero);
+    if(numero.length > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento;
-    console.log(this.complemento);
+    // console.log(this.complemento);
+    if(complemento.length > 0) {
+      this.complementoValido = true;
+    } else {
+      this.complementoValido = false;
+    }
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
-    console.log(this.formaPagamento);
+    // console.log(this.formaPagamento);
+    if(formaPagamento.length > 0) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 
 }
