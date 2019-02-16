eb8e8c547d323a0c2ea6fa2bb97920e9ae3dbbcf
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-02-16 16:13:22 -0200

    Cleanup

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -26,7 +26,7 @@ export class OrdemCompraComponent implements OnInit {
 
   public confirmarCompra(): void {
     // console.log(this.formulario.value);
-    let pedido: Pedido = new Pedido(
+    const pedido: Pedido = new Pedido(
       this.formulario.value.endereco,
       this.formulario.value.numero,
       this.formulario.value.complemento,
