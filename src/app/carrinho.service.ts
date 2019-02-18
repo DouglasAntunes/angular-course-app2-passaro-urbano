f50283f9b24056a286ba5946b0ba13a8a5b73957
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-02-18 18:09:17 -0300

    Aula 221

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -28,6 +28,12 @@ class CarrinhoService {
             this.itens.push(itemCarrinho);
         }
     }
+
+    public totalCarrinhoCompras(): number {
+        let total: number = 0;
+        this.itens.map((item: ItemCarrinho) => total += item.valor * item.quantidade);
+        return total;
+    }
 }
 
 export { CarrinhoService };
